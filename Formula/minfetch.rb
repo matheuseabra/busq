0d804b28a261f05e6d07a173e761112369b29bb1
Matheus Seabra <matheusvieiracoelho@gmail.com>
2026-08-23 21:03:36 -0300

    brew: update minfetch to v0.4.1

--- Formula/minfetch.rb
+++ Formula/minfetch.rb
@@ -1,8 +1,8 @@
 class Minfetch < Formula
   desc "Tiny, pane-aware system-info readout for terminals"
   homepage "https://github.com/matheuseabra/minfetch"
-  url "https://github.com/matheuseabra/minfetch/archive/refs/tags/v0.4.0.tar.gz"
-  sha256 "07aa3de5d8c25a23b566c41ee22a0760a7a5104c49a0100ebb9ccf064e439e0f"
+  url "https://github.com/matheuseabra/minfetch/archive/refs/tags/v0.4.1.tar.gz"
+  sha256 "ef3eee30f6e1b36a0014ec365573759f5c709fcae0211eb6d4242e18769568c0"
   license "MIT"
 
   depends_on "rust" => :build
