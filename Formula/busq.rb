805fe958464423288fe28ffa52a4c9159ad99257
Matheus Seabra <matheusvieiracoelho@gmail.com>
2026-08-24 12:24:06 -0300

    brew: update formula for v1.1.0

--- Formula/busq.rb
+++ Formula/busq.rb
@@ -1,8 +1,8 @@
 class Busq < Formula
   desc "Tiny, pane-aware system-info readout for terminals"
   homepage "https://github.com/matheuseabra/busq"
-  url "https://github.com/matheuseabra/busq/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "ed5b26d224424af31fb2297fb63d07309cba48b87179d514e73b1926af69b6d9"
+  url "https://github.com/matheuseabra/busq/archive/refs/tags/v1.1.0.tar.gz"
+  sha256 "c4d51a205956a459b933dff0f959bdc46a9eaac6f64aa7f8748914fe3dc202b8"
   license "MIT"
 
   depends_on "rust" => :build
