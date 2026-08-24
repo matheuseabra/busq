21b877b4f7855f9fa93f411fd39590823ac3ac0e
Matheus Seabra <matheusvieiracoelho@gmail.com>
2026-08-24 13:24:09 -0300

    brew: refresh v1.2.0 checksum

--- Formula/busq.rb
+++ Formula/busq.rb
@@ -2,7 +2,7 @@ class Busq < Formula
   desc "Tiny, pane-aware system-info readout for terminals"
   homepage "https://github.com/matheuseabra/busq"
   url "https://github.com/matheuseabra/busq/archive/refs/tags/v1.2.0.tar.gz"
-  sha256 "8f81c5f9baf7c6ac6851476cc450242cfe11f991c968ac0f456103b6437eeb83"
+  sha256 "343340cd662d2dbc46e5efb05ee1b82b5ed2380ed5a2539812fe768c5e05afed"
   license "MIT"
 
   depends_on "rust" => :build
